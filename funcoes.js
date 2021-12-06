d326f57a2e8c7f07a5c68a7737cd299ce6917016
Jhony <jhony.v.m@hotmail.com>
2021-12-06 16:57:09 -0300

    Inicio do projeto

---
+++ funcoes.js
@@ -0,0 +1,527 @@
+var pacotesJson = `
+[
+{
+    "id": "1109",
+    "workorigemk": "C:|TEMP|ESOC1109_2021_03_01|ITZK908390 - Workbench|K908390.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K908390.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1109_2021_03_01|ITZK908390 - Workbench|R908390.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R908390.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1109_2021_03_01|ITZK908392 - Customizing|K908392.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K908392.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1109_2021_03_01|ITZK908392 - Customizing|R908392.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R908392.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1124",
+    "workorigemk": "C:|TEMP|ESOC1124|ITZK908906 - Workbench|K908906.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K908906.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1124|ITZK908906 - Workbench|R908906.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R908906.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1124|ITZK908941 - Customizing|K908941.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K908941.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1124|ITZK908941 - Customizing|R908941.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R908941.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1125",
+    "workorigemk": "C:|TEMP|ESOC1125|ITZK908959 - Workbench|K908959.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K908959.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1125|ITZK908959 - Workbench|R908959.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R908959.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1125|ITZK908974 - Customizing|K908974.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K908974.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1125|ITZK908974 - Customizing|R908974.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R908974.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1126",
+    "workorigemk": "C:|TEMP|ESOC1126|ITZK909001 - Workbench|K909001.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909001.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1126|ITZK909001 - Workbench|R909001.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909001.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1126|ITZK909007 - Customizing|K909007.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909007.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1126|ITZK909007 - Customizing|R909007.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909007.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1127",
+    "workorigemk": "C:|TEMP|ESOC1127|ITZK909013 - Workbench|K909013.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909013.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1127|ITZK909013 - Workbench|R909013.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909013.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1127|ITZK909015 - Customizing|K909015.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909015.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1127|ITZK909015 - Customizing|R909015.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909015.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1128",
+    "workorigemk": "C:|TEMP|ESOC1128|ITZK909040 - Workbench|K909040.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909040.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1128|ITZK909040 - Workbench|R909040.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909040.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1128|ITZK909041 - Customizing|K909041.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909041.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1128|ITZK909041 - Customizing|R909041.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909041.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1129",
+    "workorigemk": "C:|TEMP|ESOC1129|ITZK909074 - Workbench|K909074.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909074.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1129|ITZK909074 - Workbench|R909074.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909074.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1129|ITZK909075 - Customizing|K909075.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909075.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1129|ITZK909075 - Customizing|R909075.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909075.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1130",
+    "workorigemk": "C:|TEMP|ESOC1130|ITZK909107 - Workbench|K909107.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909107.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1130|ITZK909107 - Workbench|R909107.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909107.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1130|ITZK909086 - Customizing|K909086.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909086.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1130|ITZK909086 - Customizing|R909086.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909086.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1131",
+    "workorigemk": "C:|TEMP|ESOC1131|ITZK909147 - Workbench|K909147.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909147.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1131|ITZK909147 - Workbench|R909147.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909147.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1131|ITZK909159 - Customizing|K909159.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909159.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1131|ITZK909159 - Customizing|R909159.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909159.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1132",
+    "workorigemk": "C:|TEMP|ESOC1132|ITZK909167 - Workbench|K909167.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909167.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1132|ITZK909167 - Workbench|R909167.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909167.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1132|ITZK909170 - Customizing|K909170.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909170.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1132|ITZK909170 - Customizing|R909170.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909170.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1133",
+    "workorigemk": "C:|TEMP|ESOC1133|ITZK909202 - Workbench|K909202.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909202.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1133|ITZK909202 - Workbench|R909202.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909202.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1133|ITZK909203 - Customizing|K909203.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909203.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1133|ITZK909203 - Customizing|R909203.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909203.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+},
+{
+    "id": "1134",
+    "workorigemk": "C:|TEMP|ESOC1134|ITZK909219 - Workbench|K909219.ITZ",
+    "workdestinok": "/usr/sap/trans/cofiles/K909219.ITZ",
+
+    "workorigemr": "C:|TEMP|ESOC1134|ITZK909219 - Workbench|R909219.ITZ",
+    "workdestinor": "/usr/sap/trans/data/R909219.ITZ",
+    
+    "custorigemk": "C:|TEMP|ESOC1134|ITZK909222 - Customizing|K909222.ITZ",
+    "custdestinok": "/usr/sap/trans/cofiles/K909222.ITZ",
+
+    "custorigemr": "C:|TEMP|ESOC1134|ITZK909222 - Customizing|R909222.ITZ",
+    "custdestinor": "/usr/sap/trans/data/R909222.ITZ",
+    
+    "packagelink": "",
+    "prereqslink": "",
+
+    "lastupdate": ""
+}]`;
+
+var pacotes = JSON.parse(pacotesJson)
+
+function geraJson() {
+
+    let htmlW, htmlC
+    let dir = document.getElementById("dir").value
+    let fullPath = prompt("Caminho completo", "C:/TEMP/ESOC1122/WORKBENCH ou CUSTOMIZING")
+    let path = fullPath.substr(0, fullPath.lastIndexOf(`ITZ`))
+
+    let package = parseInt(fullPath.substr(fullPath.lastIndexOf(`ITZ`) + 4, 10))
+
+    htmlW = `
+        <p>
+        ${fullPath}\\K${package}.ITZ   ->    ${dir}/cofiles/K${package}.ITZ
+        <br>
+        ${fullPath}\\R${package}.ITZ   ->    ${dir}/data/R${package}.ITZ
+        </p>
+    `;
+
+    htmlC = `
+        <p>
+            ${path}ITZK${package + 1}\\K${package + 1}.ITZ   ->    ${dir}/cofiles/K${package + 1}.ITZ
+            <br>
+            ${path}ITZK${package + 1}\\R${package + 1}.ITZ   ->    ${dir}/data/R${package + 1}.ITZ
+        </p>
+    `;
+
+    document.getElementById("workbench").innerHTML = htmlW
+    // document.getElementById("customizing").innerHTML = htmlC
+
+}
+
+function exibe() {
+
+    // transicaoFade()
+
+    let html = `<table id="tabelaJson" class="table table-striped table-bordered" style="width:100%">
+      <thead>
+          <tr>
+            <th>Pacote</th>
+            <th>Request</th>
+            <th>Origem Cofiles</th>
+            <th>Destino Cofiles</th>
+            <th>Origem Data</th>
+            <th>Destino Data</th>     
+          </tr>
+      </thead>
+      <tbody>`;
+
+    // //   <th>Download</th>
+    // //   <th>Pré-reqs</th>
+    // //   <th>Data Envio</th>
+
+    for (let i = 0; i < pacotes.length; i++) {
+
+        let wok = pacotes[i].workorigemk.substr(pacotes[i].workorigemk.lastIndexOf("|") + 1, pacotes[i].workorigemk.length)
+        let wor = pacotes[i].workorigemr.substr(pacotes[i].workorigemr.lastIndexOf("|") + 1, pacotes[i].workorigemr.length)
+        let cok = pacotes[i].custorigemk.substr(pacotes[i].custorigemk.lastIndexOf("|") + 1, pacotes[i].custorigemk.length)
+        let cor = pacotes[i].custorigemr.substr(pacotes[i].custorigemr.lastIndexOf("|") + 1, pacotes[i].custorigemr.length)
+
+        html += `
+        <tr>
+            <td>${pacotes[i].id}</td>
+            <td>W - ${pacotes[i].workorigemk.substr(pacotes[i].workorigemk.indexOf("ITZ"), 10)} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'PKTW');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${wok} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'WOK');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${pacotes[i].workdestinok} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'WDK');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${wor} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'WOR');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${pacotes[i].workdestinor} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'WDR');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+        </tr>`
+
+        html += `
+
+        <tr>
+            <td>${pacotes[i].id}</td>
+            <td>C - ${pacotes[i].custorigemk.substr(pacotes[i].custorigemk.indexOf("ITZ"), 10)} <button onclick="copyToClipboard(${i}, 'PKTC');myFunction.call(this)" class="btn btn-primary"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${cok} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'COK');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${pacotes[i].custdestinok} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'CDK');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${cor} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'COR');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+            <td>${pacotes[i].custdestinor} <button class="btn btn-primary" onclick="copyToClipboard(${i}, 'CDR');myFunction.call(this)"><i class="fa fa-clipboard" aria-hidden="true"></i></button></td>
+        </tr>
+        `;
+
+        //     // <td>${pacotes[i].packagelink}</td>
+        //     //     <td>${pacotes[i].prereqslink}</td>
+        //     //     <td>${pacotes[i].lastupdate}</td>
+
+    }
+
+    html += `</tbody></table>`
+
+    document.getElementById("table").innerHTML = html
+
+    setTimeout(() => { //desabilita o botão por 3 segundos
+
+    carregaTabelaDatatables()
+
+    }, 50);
+
+}
+
+function myFunction() {
+    $(this).toggleClass('buttonClassB');
+};
+
+function copyToClipboard(i, type) {
+
+    let value = ""
+
+    switch (type) {
+        case "PKTW":
+            value = pacotes[i].workorigemk.substr(pacotes[i].workorigemk.indexOf("ITZ"), 10)
+            break;
+        case "PKTC":
+            value = pacotes[i].custorigemk.substr(pacotes[i].custorigemk.indexOf("ITZ"), 10)
+            break;
+        case "WOK":
+            value = pacotes[i].workorigemk
+            break;
+        case "WDK":
+            value = pacotes[i].workdestinok
+            break;
+        case "WOR":
+            value = pacotes[i].workorigemr
+            break;
+        case "WDR":
+            value = pacotes[i].workdestinor
+            break;
+        case "COK":
+            value = pacotes[i].custorigemk
+            break;
+        case "CDK":
+            value = pacotes[i].custdestinok
+            break;
+        case "COR":
+            value = pacotes[i].custorigemr
+            break;
+        case "CDR":
+            value = pacotes[i].custdestinor
+            break;
+
+        default:
+            value = "erro"
+            break;
+    }
+
+    $("#myInput").val(value.replaceAll("|", "\\"))
+
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+    // alert("CTRL+C efetuado: " + copyText.value);
+
+    playSoungEffect(1)
+}
+
+function playSoungEffect(effect) {
+
+    let audio
+
+    if (effect == 1) { //success
+        audio = new Audio('resources/audio/mixkit-software-interface-start.wav');
+    } else if (effect == 2) { //error
+        audio = new Audio('resources/audio/mixkit-software-interface-remove.wav');
+    } else if (effect == 3) { //warning
+        audio = new Audio('resources/audio/mixkit-software-interface-back.wav');
+    }
+
+    audio.volume = 0.1;
+
+    audio.play();
+}
+
+function transicaoFade() {
+    //  efeito fade ao abrir tabela
+    document.getElementById("mainContent").style.display = "none";
+    $('#mainContent').fadeIn(500);
+}
+
+function carregaTabelaDatatables() {
+
+    $.extend(true, $.fn.dataTable.defaults, {
+        "lengthMenu": [10, 25, 50, 75, 100],
+
+        oLanguage: {
+            "sProcessing": "Processando...",
+            "sLengthMenu": "Mostrar _MENU_ registros",
+            "sZeroRecords": "Não há resultados",
+            "sEmptyTable": "Não há registros na tabela",
+            "sInfo": "Mostrando registros de _START_ a _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando registros de 0 a 0 de um total de 0 registros",
+            "sInfoFiltered": "(filtrado de um total de _MAX_ registros)",
+            "sInfoPostFix": "",
+            "sSearch": "Buscar: ",
+            "sUrl": "",
+            "sInfoThousands": ",",
+            "sLoadingRecords": "Carregando...",
+            "oPaginate": {
+                "sFirst": "Primeiro",
+                "sLast": "Último",
+                "sNext": "Seguinte",
+                "sPrevious": "Anterior"
+            }
+        },
+
+        lengthMenu: [
+            [10, 25, 50, 100, -1],
+            [10, 25, 50, 100, "Todos"]
+        ],
+
+        buttons: [
+            // 'copy', 'csv', 'excel', 'pdf', 'print'
+
+            {
+                text: 'Copiar',
+                extend: 'copy',
+                className: 'btn btn-primary'
+            },
+            {
+                text: 'CSV',
+                extend: 'csv',
+                className: 'btn btn-primary'
+            },
+            {
+                text: 'Excel',
+                extend: 'excel',
+                className: 'btn btn-primary'
+            },
+            {
+                text: 'PDF',
+                extend: 'pdf',
+                className: 'btn btn-primary'
+            },
+            {
+                text: 'Imprimir',
+                extend: 'print',
+                className: 'btn btn-primary'
+            }
+        ],
+
+        language: {
+            searchPlaceholder: "digite aqui",
+            buttons: {
+                copyTitle: 'Copiou para área de transferência',
+                copyKeys: 'Press <i>ctrl</i> or <i>\u2318</i> + <i>C</i> to copy the table data<br>to your system clipboard.<br><br>To cancel, click this message or press escape.',
+                copySuccess: {
+                    _: '%d linhas copiadas',
+                    1: '1 linha copiada'
+                }
+            }
+        },
+
+        // 'sDom': 't', //oculta botões, barra de pesquisa e paginação
+
+        responsive: false,
+        lengthChange: true,
+        searching: true
+
+    });
+
+    $(document).ready(function () {
+
+        tabelaGerada =
+        $('#tabelaJson').DataTable({
+            "order": [
+                [0, "desc"]
+            ], //asc ou desc, index com início 0
+
+            // columnDefs: [{
+            //         responsivePriority: 1,
+            //         targets: 0
+            //     },
+            //     {
+            //         responsivePriority: 2,
+            //         targets: 1
+            //     },
+            //     {
+            //         responsivePriority: 3,
+            //         targets: 5
+            //     },
+            //     {
+            //         targets: [5, 6, 7],
+            //         orderable: false
+            //     }
+            // ]
+
+        });
+
+        // $("#tabelaJson_length").appendTo("#exibirQtd")
+        // $("#tabelaJson_filter").appendTo("#exibirQtd")
+
+    });
+
+}
+
+window.onload = exibe();
